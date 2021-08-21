1179601beb1cec760af906993ad8c8ee8322763f
Marcono1234 <Marcono1234@users.noreply.github.com>
2021-08-22 00:46:42 +0200

    #4: Create `databases` output directory before starting Docker container

--- build_database.cmd
+++ build_database.cmd
@@ -1,2 +1,5 @@
+@REM Create output directory; abort if directory cannot be created (e.g. when file with this name exists)
+IF NOT EXIST "databases/" mkdir databases || EXIT 1
+
 @REM Build image, then start container (with `--rm` to remove it once finished)
 docker build . -t codeql-jdk && docker container run --rm --name "codeql-jdk-db-build" --mount type=bind,source="%cd%/databases",target=/codeql-jdk/codeql-jdk-databases codeql-jdk %*
--- build_database.sh
+++ build_database.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Create output directory
+mkdir -p databases
 # Build image, then start container (with `--rm` to remove it once finished)
 docker build . -t codeql-jdk
 docker container run --rm --name "codeql-jdk-db-build" --mount type=bind,source="$(pwd)/databases",target=/codeql-jdk/codeql-jdk-databases codeql-jdk "$@"
